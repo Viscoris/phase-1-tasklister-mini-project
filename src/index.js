158817cdc9e232487c4b0bd63f6ffc888375b8f3
Daniel Waghorne <dwaghorne@hotmail.com>
2024-01-27 09:44:53 +1000

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("form")
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+    let initialValue = document.getElementById("new-task-description")
+    handleToDo(initialValue.value)
+    form.reset()
+  })
+})
+
+function handleToDo(toDo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "x"
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  btn.addEventListener("click", handleDelete)
+  document.getElementById("list").appendChild(p)
+}
+
+let handleDelete = e => e.target.parentNode.remove()
